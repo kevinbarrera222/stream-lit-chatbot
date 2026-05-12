0715edcbcebf82783dc956064def876a431ab3df
kevinbarrera222 <kevinbarrera222@gmail.com>
2026-05-11 21:23:24 -0500

    feat: configuración inicial de la interfaz y modelo del chatbot

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -2,4 +2,11 @@ from langchain_openai import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 import streamlit as st
 
+#Configurar la pagina de la app 
+st.set_page_config(page_title="Chatbot Básico", page_icon="🤖")
+st.title("🤖Chatbot Básico con Lanchaing")
+st.markdown("Este es un *chatbot de ejemplo* construido con Langchain + Streamlit. ¡Escribe tu mensaje abajo para comenzar!")
+
+chat_model = ChatOpenAI(model="gpt-4o-mini", temperature=0.5)
+
 
