e84f9036c2e572d916999b64ff86caa274750c0d
kevinbarrera222 <kevinbarrera222@gmail.com>
2026-05-12 18:10:01 -0500

    feat: agregar panel de configuración, prompt personalizado y reinicio de conversación

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -1,18 +1,40 @@
 from langchain_openai import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 import streamlit as st
+from langchain.prompts import PromptTemplate 
 
 #Configurar la pagina de la app 
 st.set_page_config(page_title="Chatbot Básico", page_icon="🤖")
 st.title("🤖Chatbot Básico con Lanchaing")
 st.markdown("Este es un *chatbot de ejemplo* construido con Langchain + Streamlit. ¡Escribe tu mensaje abajo para comenzar!")
 
+with st.sidebar:
+    st.header("Configuración")
+    temperature = st.slider("Temperatura",0.0, 1.0, 0.5, 0.1)
+    model_name = st.selectbox("Modelo",["gtp-3.5-turbo","gtp-4","gpt-40-mini"])
+
+    # Recrea el modelo con nuevos parametros
 chat_model = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
 
 
 # Inicializar el historial de mensajes
 if "mensajes" not in st.session_state:
     st.session_state.mensajes = []
+
+
+ #crear el template de prompt con comportamiento especifico
+
+Prompt_template = PromptTemplate(
+    input_variables=["mensaje","historial"],
+    template="""Eres un asistente util y amigable llamdo Chatbot Pro.
+
+Historial de conversación:
+{historial}
+Responde de manera clara y concisa a la siguiente pregunta: {mensaje}"""
+)
+
+# Crear cadena usando LCEL (Langchain Expression Language)
+cadena = Prompt_template | chat_model
 
 
 # Mostrar mensajes previos en la interfaz 
@@ -25,6 +47,10 @@ for msg in st.session_state.mensajes:
 
     with st.chat_message(role):
         st.markdown(msg.content)
+
+if st.button("🗑️Nueva conversación"):
+    st.session_state.mensajes = []
+    st.rerun()
 
 # Cuadro de entrada de texto de usuario 
 
